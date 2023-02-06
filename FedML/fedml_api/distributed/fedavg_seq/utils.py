e77addc8e98f861d55f73ee3228056f6d38f7e01
Tuo Zhang <tuozhang@usc.edu>
2023-02-05 17:45:32 -0800

    Update utils.py

--- FedML/fedml_api/distributed/fedavg_seq/utils.py
+++ FedML/fedml_api/distributed/fedavg_seq/utils.py
@@ -1,3 +1,6 @@
+# based on the original fedavg_seq inside the fedml repo (https://github.com/FedML-AI/FedML/tree/master/python/fedml/simulation/mpi/fedavg_seq), 
+# we add the sequential training on top of the fedavg, which means that each process could represent multiple clients during the training.
+
 import os
 
 import numpy as np
