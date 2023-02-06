c5991cbcc2782d5fe6cf00d1fd65ccf6607d3bcf
Tuo Zhang <tuozhang@usc.edu>
2023-02-05 17:45:04 -0800

    Update MyModelTrainer.py

--- FedML/fedml_api/distributed/fedavg_seq/MyModelTrainer.py
+++ FedML/fedml_api/distributed/fedavg_seq/MyModelTrainer.py
@@ -1,3 +1,7 @@
+# based on the original fedavg_seq inside the fedml repo (https://github.com/FedML-AI/FedML/tree/master/python/fedml/simulation/mpi/fedavg_seq), 
+# we add the sequential training on top of the fedavg, which means that each process could represent multiple clients during the training.
+
+
 import logging
 
 import torch
