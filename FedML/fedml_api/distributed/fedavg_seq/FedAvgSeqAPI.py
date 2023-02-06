5201eb87d9266e26c4baaa1d9da9e22092b1e7fe
Tuo Zhang <tuozhang@usc.edu>
2023-02-05 20:18:25 -0800

    Update FedAvgSeqAPI.py

--- FedML/fedml_api/distributed/fedavg_seq/FedAvgSeqAPI.py
+++ FedML/fedml_api/distributed/fedavg_seq/FedAvgSeqAPI.py
@@ -1,7 +1,5 @@
-# based on the original fedavg_seq inside the FedML repo (https://github.com/FedML-AI/FedML/tree/master/python/fedml/simulation/mpi/fedavg_seq)
-# and one of the forked version of FedML repo (https://github.com/wizard1203/FedML/tree/master/python/fedml/simulation/mpi/fedavg_seq)
-# we add the sequential training on top of the fedavg, which means that each process could represent multiple clients during the training.
-# we also change the test function for the F1 score test.
+# This file is a modified version of FedAvgSeqAPI.py (https://github.com/wizard1203/FedML/blob/master/python/fedml/simulation/mpi/fedavg_seq/FedAvgSeqAPI.py)
+# where we changed the import path, changed the name of client_trainer to model_trainer, and added an parameter named preprocessed_sampling_lists to FedML_FedAvgSeq_distributed
 
 
 from mpi4py import MPI
