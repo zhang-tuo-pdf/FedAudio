e45fec96946b0178f75845b014506e24f815dc60
Tuo Zhang <tuozhang@usc.edu>
2023-02-05 20:09:40 -0800

    Update FedAVGTrainer.py

--- FedML/fedml_api/distributed/fedavg_seq/FedAVGTrainer.py
+++ FedML/fedml_api/distributed/fedavg_seq/FedAVGTrainer.py
@@ -1,7 +1,5 @@
-# based on the original fedavg_seq inside the FedML repo (https://github.com/FedML-AI/FedML/tree/master/python/fedml/simulation/mpi/fedavg_seq)
-# and one of the forked version of FedML repo (https://github.com/wizard1203/FedML/tree/master/python/fedml/simulation/mpi/fedavg_seq)
-# we add the sequential training on top of the fedavg, which means that each process could represent multiple clients during the training.
-# we also change the test function for the F1 score test.
+# This file is a modified version of FedAVGTrainer.py (https://github.com/wizard1203/FedML/blob/master/python/fedml/simulation/mpi/fedavg_seq/FedAVGTrainer.py)
+# where we introduced a new function update_round and a new parameter input at line 49.
 
 
 from .utils import transform_tensor_to_list
